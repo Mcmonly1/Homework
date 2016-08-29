9de9ed07795bc1a2ec72a1e6a8b988ed34b91cfc
Mcmonly1 <rentalbeast@yahoo.com>
2016-08-29 18:04:15 -0500

    update
    uypdate

--- src/Week4/Lab1/Assignment.java
+++ src/Week4/Lab1/Assignment.java
@@ -2,33 +2,27 @@ package Week4.Lab1;
 
 public class Assignment {
 
-	private double score = 75;
+	private double score;
 	
 private String CalculateGrade(double scoreGrade)
 {
 	String grade = null;
 	if(score > 90)
-	{
 		grade = "A";
-		return grade;
-	}
 	
 	if (score >= 80 && score < 90)
-	{
 		grade = "B";
-		return grade;
-	}
 	
 	if (score >= 70 && score < 80)
-	{
 		grade = "C";
-		return grade;
-	}
+	
+	if (score >= 60 && score < 70)
+		grade = "D";
+	
 	else
-	{
 		grade = "F";
-		return grade;
-	}
+	
+	return grade;
 }
 
 private double getScore(){
