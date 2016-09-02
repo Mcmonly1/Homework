581ef47955898cae1f12392fa3c71606f1e17a72
Mcmonly1 <rentalbeast@yahoo.com>
2016-09-02 15:44:11 -0500

    error fixes

--- src/Week4/Lab1/Exam.java
+++ src/Week4/Lab1/Exam.java
@@ -1,11 +1,11 @@
 package Week4.Lab1;
 
-public class Exam {
+public class Exam extends Assignment {
 	int questions;
 	float points;
 	int missed;
 	
-	public float exam(int questions, int missed){
+	public void exam(int questions, int missed){
 		points = 100 / (questions - missed);
 		super.setScore(points);
 	}
