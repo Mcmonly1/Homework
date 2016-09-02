3258eeb905481cd171a6764dc27f01299d66f643
Mcmonly1 <rentalbeast@yahoo.com>
2016-09-02 16:07:30 -0500

    task 5

--- src/Week4/Lab1/Exam.java
+++ src/Week4/Lab1/Exam.java
@@ -2,18 +2,30 @@ package Week4.Lab1;
 
 public class Exam extends Assignment {
 	int questions;
-	float points;
+	static float points;
 	int missed;
 	
-	public void exam(int questions, int missed){
+	public void examCalculator(int questions, int missed){
 		points = 100 / (questions - missed);
 		super.setScore(points);
 	}
 
-	public float getPoints(){
+	public float getPoints() {
 		return points;
 	}
-	public int getMissed(){
+	public int getMissed() {
 		return missed;
+	}
+	public int getQuestions() {
+		return questions;
+	}
+	public void setQuestions (int value) {
+		questions = value;
+	}
+	public void setMissed (int value) {
+		missed = value;
+	}
+	public void setPoints (float value) {
+		points = value;
 	}
 }
--- src/Week4/Lab1/Main.java
+++ src/Week4/Lab1/Main.java
@@ -6,12 +6,23 @@ public class Main {
 
 	public static void main(String[] args) {
 		Scanner userInput = new Scanner(System.in);
+		
 		float score;
+		int questions;
+		int missed;
+		
+		Exam exam = new Exam();
 		Assignment assignment = new Assignment();
 		
-		System.out.println("Please submit the score you achieved.");
-		score = userInput.nextFloat();
-		assignment.setScore(score);
+		System.out.println("How many questions were on the exam?");
+		questions = userInput.nextInt();
+		System.out.println("How many questions did you miss on the exam?");
+		missed = userInput.nextInt();
+		exam.setQuestions(questions);
+		exam.setMissed(missed);
+		exam.examCalculator(questions, missed);
+// Lost on how to get points to assignment class and still be able to display points for each question in console.
+		exam.setPoints(points);
 		assignment.CalculatedGrade(assignment.getGrade());
 		System.out.println("The grade you achieved was a " + assignment.CalculatedGrade(assignment.getScore()));
 		userInput.close();
