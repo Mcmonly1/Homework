30005930a98b5f9d3b6cbe5c737d4c384c273c33
James <zaleus3@gmail.com>
2016-09-03 01:37:41 -0500

    A thing was done

--- src/Week5/Lab1/week5lab1.java
+++ src/Week5/Lab1/week5lab1.java
@@ -3,6 +3,10 @@ package Week5.Lab1;
 import java.util.Scanner;
 
 public class week5lab1 {
+	private static float num1;
+	private static float num2;
+	private static int taipe;
+	private static boolean wrongletter = false;
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
@@ -17,45 +21,69 @@ public class week5lab1 {
 				+ "\n\t D.) Divide.");
 		String userChoice = userInput.next();
 		if (userChoice.length()== 1) {
-			char letter = userChoice.toCharArray()[0];
-		System.out.println("What is the first number you want to use?");
-		float num1 = userInput.nextFloat();
-		System.out.println("What is the second number you want to use?");
-		float num2 = userInput.nextFloat();
-		
+			char letter = userChoice.toCharArray()[0];	
 		switch(letter) {
-		
 		case 'A':
 		case 'a':
-			float answerA = week5lab1.addition(num1, num2);
-			System.out.println("your answer is " + answerA);
+			selectMathType(1);
 			break;
 			
 		case 'B':
 		case 'b':
-			float answerB = week5lab1.subtraction(num1, num2);
-			System.out.println("your answer is " + answerB);
+			selectMathType(2);
 			break;
 		
 		case 'C':
 		case 'c':
-			float answerC = week5lab1.multiply(num1, num2);
-			System.out.println("your answer is " + answerC);
+			selectMathType(3);
 			break;
 			
 		case 'D':
 		case 'd':
-			float answerD = week5lab1.divide(num1, num2);
-			System.out.println("your answer is " + answerD);
+			selectMathType(4);
 			break;
 			
 		default :
 			System.out.println("you toopid put a, b, c, or d, not other letter");
+			wrongletter = true;
 			break;
 		}
+		if(!wrongletter)
+		{
+		System.out.println("What is the first number you want to use?");
+		num1 = userInput.nextFloat();
+		System.out.println("What is the second number you want to use?");
+		num2 = userInput.nextFloat();
+		doMath();
 		}
-		else 
+		}
+		else
+		{
 			System.out.println("you toopid put a, b, c, or d, not other letter");
+			wrongletter = false;
+		}
+	}
+	public static void selectMathType(int i) {
+		taipe = i;
+	}
+	public static void doMath() {
+		float answer = 0;
+		switch(taipe)
+		{
+		case 1:
+			answer = week5lab1.addition(num1, num2);
+			break;
+		case 2:
+			answer = week5lab1.subtraction(num1, num2);
+			break;
+		case 3:
+			answer = week5lab1.multiply(num1, num2);
+			break;
+		case 4:
+			answer = week5lab1.divide(num1, num2);
+			break;
+		}
+		System.out.println("your answer is " + answer);
 	}
 	public static float addition(float num1, float num2) {
 		float answer;
